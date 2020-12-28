b0e064d270adf1cff5b29c7fe538e4b3275dea7e
andrii-lavrenchuk <63540148+andrii-lavrenchuk@users.noreply.github.com>
2020-12-28 14:23:09 +0200

    Create Notification.js

---
+++ src/components/Notification/Notification.js
@@ -0,0 +1,10 @@
+import PropTypes from 'prop-types';
+import s from './Notification.module.css';
+
+export default function Notification({ message }) {
+  return <h2 className={s.title}>{message}</h2>;
+}
+
+Notification.propTypes = {
+  message: PropTypes.string.isRequired,
+};
